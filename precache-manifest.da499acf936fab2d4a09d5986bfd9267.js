50a0220b8ae176bcd7334092f7b33cda2453b0cf
fab4u-4empion <reserv390@gmail.com>
2021-05-02 15:46:03 +0300

    Updates

---
+++ precache-manifest.da499acf936fab2d4a09d5986bfd9267.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17c48f4b46c9fae30cb9f58dcc4d4571",
+    "url": "/VKAppTournament/index.html"
+  },
+  {
+    "revision": "6b2c48c57478dbc9dd1e",
+    "url": "/VKAppTournament/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "de71b31c496b00117ecb",
+    "url": "/VKAppTournament/static/css/main.6eca340c.chunk.css"
+  },
+  {
+    "revision": "6b2c48c57478dbc9dd1e",
+    "url": "/VKAppTournament/static/js/2.b8252de9.chunk.js"
+  },
+  {
+    "revision": "84c0fb3b5e9c985532d69c2f9c3eba5b",
+    "url": "/VKAppTournament/static/js/2.b8252de9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de71b31c496b00117ecb",
+    "url": "/VKAppTournament/static/js/main.ee1b0bab.chunk.js"
+  },
+  {
+    "revision": "6791e3fa56dfad7ba035",
+    "url": "/VKAppTournament/static/js/runtime-main.b882065a.js"
+  }
+]);
