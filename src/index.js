890cd6b465b5cf7f3aa9ef7b8a481053f083ccf4
fab4u_4empion <reserv390@gmail.com>
2021-07-26 21:27:59 +0300

    asynchronous import of "App" component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import "core-js/features/map";
 import "core-js/features/set";
+import { ScreenSpinner } from "@vkontakte/vkui";
 import React from "react";
 import ReactDOM from "react-dom";
 import bridge from "@vkontakte/vk-bridge";
 import '@vkontakte/vkui/dist/vkui.css';
-import App from "./App";
+//import App from "./App";
 import './style.css';
 
 
@@ -17,6 +18,15 @@ bridge.subscribe((e) => {
     }
 });
 
-ReactDOM.render(<App />, document.getElementById("root"));
-    import("./eruda").then(({ default: eruda }) => {}); //runtime download
+ReactDOM.render(<ScreenSpinner/>, document.getElementById("root"));
+
+async function load() {
+    let obj = await import("./App")
+    let App = obj.default
+    ReactDOM.render(<App/>, document.getElementById("root"));    
+}
+
+load()
+    
+import("./eruda").then(({ default: eruda }) => {}); //runtime download
 
